7bbb27f015e6160fbc1049e53689dc9a773e12c4
wametsol <waltteri.metsola@gmail.com>
2018-11-04 07:57:02 +0200

    10

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -22,6 +22,7 @@ public class VarastoTest {
 
     @Test
     public void konstruktoriLuoTyhjanVaraston() {
+        
         assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
     }
     @Test
