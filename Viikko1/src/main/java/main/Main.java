c19973abe3be8f58e7c17f75bff6729373d5a946
wametsol <waltteri.metsola@gmail.com>
2018-11-07 12:28:16 +0200

    v2t2

--- Viikko1/src/main/java/main/Main.java
+++ Viikko1/src/main/java/main/Main.java
@@ -3,60 +3,21 @@ package main;
 import ohtu.ohtuvarasto.Varasto;
 
 public class Main {
-
+    static final double asd = 100.0;
+    static final double asd1 = 20.2;
     public static void main(String[] args) {
-        Varasto testeri = new Varasto(10);
-        System.out.println(testeri.toString());
+        
 
-        Varasto mehua = new Varasto(100.0);
-        Varasto olutta = new Varasto(100.0, 20.2);
+        
+        Varasto mehua = new Varasto(asd);
+        Varasto olutta = new Varasto(asd, asd1);
 
         System.out.println("Luonnin jälkeen:");
         System.out.println("Mehuvarasto: " + mehua);
         System.out.println("Olutvarasto: " + olutta);
 
-        System.out.println("Olutgetterit:");
-        System.out.println("getSaldo()     = " + olutta.getSaldo());
-        System.out.println("getTilavuus    = " + olutta.getTilavuus());
-        System.out.println("paljonkoMahtuu = " + olutta.paljonkoMahtuu());
-
-        System.out.println("Mehusetterit:");
-        System.out.println("Lisätään 50.7");
-        mehua.lisaaVarastoon(50.7);
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("Otetaan 3.14");
-        mehua.otaVarastosta(3.14);
-        System.out.println("Mehuvarasto: " + mehua);
-
-        System.out.println("Virhetilanteita:");
-        System.out.println("new Varasto(-100.0);");
-        Varasto huono = new Varasto(-100.0);
-        System.out.println(huono);
-
-        System.out.println("new Varasto(100.0, -50.7)");
-        huono = new Varasto(100.0, -50.7);
-        System.out.println(huono);
-
-        System.out.println("Olutvarasto: " + olutta);
-        System.out.println("olutta.lisaaVarastoon(1000.0)");
-        olutta.lisaaVarastoon(1000.0);
-        System.out.println("Olutvarasto: " + olutta);
-
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("mehua.lisaaVarastoon(-666.0)");
-        mehua.lisaaVarastoon(-666.0);
-        System.out.println("Mehuvarasto: " + mehua);
-
-        System.out.println("Olutvarasto: " + olutta);
-        System.out.println("olutta.otaVarastosta(1000.0)");
-        double saatiin = olutta.otaVarastosta(1000.0);
-        System.out.println("saatiin " + saatiin);
-        System.out.println("Olutvarasto: " + olutta);
-
-        System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("mehua.otaVarastosta(-32.9)");
-        saatiin = mehua.otaVarastosta(-32.9);
-        System.out.println("saatiin " + saatiin);
-        System.out.println("Mehuvarasto: " + mehua);
+      
     }
+
+
 }
