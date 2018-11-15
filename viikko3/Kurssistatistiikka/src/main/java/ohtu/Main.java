61d01c2d2221b141a5c73c6bf77a55735304b792
wametsol <waltteri.metsola@gmail.com>
2018-11-15 17:23:05 +0200

    T1 - toimii

--- viikko3/Kurssistatistiikka/src/main/java/ohtu/Main.java
+++ viikko3/Kurssistatistiikka/src/main/java/ohtu/Main.java
@@ -1,7 +1,38 @@
 package ohtu;
 
+import com.google.gson.Gson;
+import java.io.IOException;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // ÄLÄ laita githubiin omaa opiskelijanumeroasi
+        String studentNr = "012345678";
+        if ( args.length>0) {
+            studentNr = args[0];
+        }
+
+        String url = "https://studies.cs.helsinki.fi/courses/students/"+studentNr+"/submissions";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        System.out.println("Opiskelijanumero: " + studentNr);
+        //System.out.println( bodyText );
+
+        Gson mapper = new Gson();
+        Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
+        int tunnit = 0;
+        int tehtavat = 0;
+        System.out.println(" ");
+        for (Submission submission : subs) {
+            tunnit = tunnit + submission.getHours();
+            tehtavat = tehtavat + submission.getExercises().length;
+            System.out.println(submission);
+        }
+        
+        System.out.println("");
+        System.out.println("Yhteensa: " +tehtavat + " tehtavaa, " + tunnit+ " tuntia.");
+
     }
 }
--- viikko3/Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ viikko3/Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -2,18 +2,56 @@ package ohtu;
 
 public class Submission {
     private int week;
+    private String course;
+    private int hours;
+    private int[] exercises;
 
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public int[] getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(int[] exercises) {
+        this.exercises = exercises;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
     public void setWeek(int week) {
         this.week = week;
     }
+    
 
     public int getWeek() {
         return week;
     }
+    
+    public String exerciseList(){
+        String list = "";
+        for(int i=1;i<exercises.length;i++){
+            list = list + ", " + exercises[i];
+        }
+        return list;
+    }
+    
+    
+    
 
     @Override
     public String toString() {
-        return ""+week;
+        return ""+course + ", viikko " +week + ". Aikaa kului: " + hours + ". Tehtyja tehtavia yhteensa " + exercises.length + ", tehdyt tehtavat: " + exercises[0] + exerciseList();
     }
     
 }
